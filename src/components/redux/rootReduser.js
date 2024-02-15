8fa961a32548028b0a8b2a2b5c100b1eabca54a9
KovalNatali <koval.natali.777@gmail.com>
2024-02-15 20:48:47 +0200

    hw-06

--- src/components/redux/rootReduser.js
+++ src/components/redux/rootReduser.js
@@ -1,9 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
 import constansReduser from './contacts/contacts-slice';
 import filterReduser from './filter/filter-slice';
+
+import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
   key: 'contacts',
